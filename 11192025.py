acdc315e20f022b458107bbff2681484ed66f8bd
Jay Kelly <jfkelly@ualr.edu>
2025-11-19 16:28:45 +0000

    class

---
+++ 11192025.py
@@ -0,0 +1,16 @@
+project 2 - recipe book with Object Oriented Programming
+Course evals - adding to blackboard now.
+
+Fancy Data Structures
+
+Binary trees, essentially every level doubles what was there before. 
+Root - then left or right, 20 instructions can get you to millions of potential stored nodes
+
+Binary search tree, ecery node is bigger than left children and smaller than right children. 
+Nice and orderly, efficient look up
+
+Tree balance matters, fancy stuff you can do to keep tree in check and keep it from getting out of control
+Minimize depth of your tree
+
+traversals - need to study up
+
